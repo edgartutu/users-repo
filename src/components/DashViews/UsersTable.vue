ca8faa1ab5f84db2591c412631fec7f73e4ed6c5
AAOMACH <aaomach@gmail.com>
2019-07-18 19:44:53 +0300

    User

--- src/components/DashViews/UsersTable.vue
+++ src/components/DashViews/UsersTable.vue
@@ -3,19 +3,19 @@
 	
 		<v-container >
 			<v-card class="rounded-card">
-				<v-card-text>
-					<v-text-field label="Student1" placeholder="Name" v-model="student1"></v-text-field>
-					<v-text-field label="Reg No" placeholder="Reg No 1" v-model="reg_no1"></v-text-field>
-					<v-text-field label="Student2" placeholder="Name" v-model="student2"></v-text-field>
-					<v-text-field label="Reg No" placeholder="Reg No 2" v-model="reg_no2"></v-text-field>
-					<v-text-field label="Title" placeholder="Title" v-model="title"></v-text-field>
-					<v-text-field label="Problem Statment" placeholder="Problem Statment" v-model="problem_statment"></v-text-field>
-					<v-text-field label="Abstract" placeholder="Abstract" v-model="abstract"></v-text-field>
-					<v-btn flat class="purple mx-0" @click="$refs.inputUpload.click()">Upload File</v-btn>
-					<input v-show="false" ref="inputUpload" type="file">
-					<v-btn flat class="green my-5" @click="postproposal()">Submit</v-btn>
+                <v-card-text>
+                    <v-text-field label="Student1" placeholder="Name" v-model="student1"></v-text-field>
+                    <v-text-field label="Reg No" placeholder="Reg No 1" v-model="reg_no1"></v-text-field>
+                    <v-text-field label="Student2" placeholder="Name" v-model="student2"></v-text-field>
+                    <v-text-field label="Reg No" placeholder="Reg No 2" v-model="reg_no2"></v-text-field>
+                    <v-text-field label="Project Reference Number" placeholder="Project Reference Number" v-model="project_ref"></v-text-field>
+                    <v-text-field label="Title" placeholder="Title" v-model="title"></v-text-field>
+                    <v-text-field label="Problem Statment" placeholder="Problem Statment" v-model="problem_statment"></v-text-field>
+                    <v-text-field label="Methodology" placeholder="Methodology" v-model="methodology"></v-text-field>
+                    <input type="file" name="Upload File" @change="uploadfile" />
+                    <v-btn flat class="green my-5" @click="postproposal()">Submit</v-btn>
 
-				</v-card-text>
+                </v-card-text>
 			</v-card>
 			
 		</v-container>
@@ -30,22 +30,37 @@ import axios from 'axios'
             return {
                 student1: "",
                 reg_no1: "",
+                project_ref: "",
                 student2: "",
                 reg_no2: "",
                 title: "",
                 problem_statment: "",
-                abstract: "",
-                file: "pdf"
+                methodology: "",
+                file: null
             }
         },
         methods: {
+
             postproposal() {
-                axios.post("http://127.0.0.1:5000/postproposals", {
-                    "student1": this.student1, "reg_no1": this.reg_no1, "student2": this.student2, "reg_no2": reg_no2,
-                    "title": this.title, "problem_statment": this.problem_statment, "abstract": this.abstract,
-                    "file": this.file
+                const fd = new FormData();
+                fd.append('foo', 'bar')
+                fd.append('file', this.file)
+                fd.append('student1', this.student1)
+                fd.append('reg_no', this.reg_no1)
+                fd.append('student2', this.student2)
+                fd.append('reg_no2', this.reg_no2)
+                fd.append('title', this.title)
+                fd.append('problem_statement', this.problem_statment)
+                fd.append('methodology', this.methodology)
+                fd.append('proposal_ref', this.project_ref)
+
+                axios.post("http://127.0.0.1:5000/postproposals", fd).then(response => {
+                    console.log(response)
                 })
             },
+            uploadfile(event) {
+                this.file = event.target.files[0]
+            }
         }
 }
 </script>
--- src/router/paths.js
+++ src/router/paths.js
@@ -33,7 +33,7 @@ export default [
       }
       
       else {
-        next()
+          next('/dashboard')
       }
     },
     children: [
